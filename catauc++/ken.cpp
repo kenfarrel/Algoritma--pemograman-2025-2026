3d9cafccf7aba42a14c7ed5cda208c2941b3d2eb
kenfarrel <kenfarrel1904@gmail.com>
2025-10-13 10:45:58 +0700

    YES

---
+++ catauc++/ken.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string nama ="ken";
+    int umur =18;
+    float tinggi =163;
+    bool aktif =true;
+
+    cout<<"nama:" <<nama<<endl;
+    cout<<"umur:"<<umur<<endl;
+    cout<<"tinggi:"<<tinggi<<endl;
+    cout<<"status aktif:"<<aktif<<endl;
+    return 0;
+}
