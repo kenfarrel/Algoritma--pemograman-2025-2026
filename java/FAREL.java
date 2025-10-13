be732ad40ce3e0da1027bd05a4e5590fd80d8e26
kenfarrel <kenfarrel1904@gmail.com>
2025-10-13 20:12:45 +0700

    yess

---
+++ java/FAREL.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(string[] args) {
+       string nama ="farel";
+       int umur =18;
+       double tinggi=163;
+       boolean aktif =true;
+       
+       system.out.println("Nama:" + nama);           
+       system.out.println("Umur:"+umur);
+       system.out.println("Tinggi:"+tinggi);
+       system.out.println("status aktif:"+aktif);
---
+++ php/say1.php
@@ -0,0 +1,11 @@
+<?php
+$nama="ken";
+$umur=18;
+$tinggi=163;
+$aktif=true;
+
+echo "Nama:$nama<br>";
+echo"Umur:$umur<br>";
+echo"Tinggi:$tinggi<br>;
+echo"status aktif:$aktif";
+?>
