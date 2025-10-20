611a0c588982b3745e28c3b5ec312a51e395d1d4
kenfarrel <kenfarrel1904@gmail.com>
2025-10-20 20:34:53 +0700

    yeuh

---
+++ ken_cpp/konstata.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+main()
+{
+    const double PI = 3.14;
+    const int NILAI_MAX = 100;
+    const char Mychar = 'A';
+    const string MyName ="Muhammad ken";
+
+    cout<<"---BELAJAR KONSTANTA---"<<endl;
+    cout<<"Konstanta PI:"<<PI<<endl;
+    cout<<"Konstanta NILAI_MAX:"<<NILAI_MAX<<endl;
+    cout<<"Konstanta Mychar:"<<Mychar<<endl;
+    cout<<"Konstanta MyName:"<<MyName<<endl;
+
+    cout<<endl;
+}
