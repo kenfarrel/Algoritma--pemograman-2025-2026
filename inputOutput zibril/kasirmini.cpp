bda77063c6eed15e72721cf61e37040f96d66d15
kenfarrel <kenfarrel1904@gmail.com>
2025-11-24 10:31:32 +0700

    tugas kasir mini

---
+++ inputOutput zibril/kasirmini.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama;
+    double harga, total, total_bayar, diskon;
+    int jumlah;
+
+    // Input data
+    cout << "Masukkan nama barang: ";
+    getline(cin, nama);
+
+    cout << "Masukkan harga barang: ";
+    cin >> harga;
+
+    cout << "Masukkan jumlah barang: ";
+    cin >> jumlah;
+
+    // Perhitungan
+    total = harga * jumlah;
+
+    if (total > 24900) {
+        diskon = total * 0.09;
+    } else {
+        diskon = 0;
+    }
+
+    total_bayar = total - diskon;
+
+    // Output Struk
+    cout << "\n===== Struk Pembayaran =====" << endl;
+    cout << "Nama Barang   : " << nama << endl;
+    cout << "Harga Barang  : " << harga << endl;
+    cout << "Jumlah Barang : " << jumlah << endl;
+    cout << "Total Harga   : " << total << endl;
+    cout << "Diskon        : " << diskon << endl;
+    cout << "Total Bayar   : " << total_bayar << endl;
+    cout << "============================" << endl;
+
+    return 0;
+}
